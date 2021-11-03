d8775384c055828dd328d9d9929f70976e0b6b68
Besenok-13 <artbondar2003@gmail.com>
2021-11-04 00:01:15 +0300

    Update

--- website/auth.py
+++ website/auth.py
@@ -5,19 +5,45 @@ Created on Wed Oct 13 21:16:14 2021
 @author: artbo
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return "<p>Login<p>"
+    data=request.form
+    print(data)
+    return render_template("login.html", text="Testing", user="Tim", boolean=True)
+
 
 @auth.route("/logout")
 def logout():
     return"<p>logout<p>"
 
-@auth.route("/sing-up")
-def sign_up():
-    return "<p>Sign up<p>"
 
+@auth.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+    if request.method=="POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters.", category='error')
+        elif len(firstName) < 2:
+            flash("firstName must be greater than 2 character.", category='error')
+
+        elif password1 != password2:
+            flash("Passwords don't match.", category='error')
+
+        elif len(password1) < 6:
+            flash("Password must be at least 6 characters.", category='error')
+
+        else:
+            # add user to database
+            flash("Accout created! Let's say hello to this EnJoy cooking world!", category="success")
+
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -5,10 +5,10 @@ Created on Wed Oct 13 21:16:55 2021
 @author: artbo
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>TestersGonnaTest<h1>"
+    return render_template("home.html")
