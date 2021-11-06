34f53da65c821f8b8d928f32d9618f6bf8f194e5
Besenok-13 <artbondar2003@gmail.com>
2021-11-06 23:27:08 +0300

    CommitUpdateDatabaseAndUser

--- website/auth.py
+++ website/auth.py
@@ -5,7 +5,10 @@ Created on Wed Oct 13 21:16:14 2021
 @author: artbo
 """
 
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 
 auth = Blueprint('auth', __name__)
@@ -27,13 +30,13 @@ def logout():
 def sign_up():
     if request.method=="POST":
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
+        first_name = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
         if len(email) < 4:
             flash("Email must be greater than 4 characters.", category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("firstName must be greater than 2 character.", category='error')
 
         elif password1 != password2:
@@ -44,6 +47,13 @@ def sign_up():
 
         else:
             # add user to database
+            new_user=User(email=email, first_name=first_name,password=generate_password_hash(password1, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Accout created! Let's say hello to this EnJoy cooking world!", category="success")
+
+
+            return redirect(url_for("views.home"))
+
 
     return render_template("sign_up.html")
--- website/models.py
+++ website/models.py
@@ -4,4 +4,25 @@ Created on Wed Oct 13 21:16:38 2021
 
 @author: artbo
 """
+from . import db
+from flask_login import UserMixin
+from sqlalchemy import func
 
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    data = db.Column(db.DateTime(timezone=True), default=func.now())
+    #   user.id пишется с маленькой буквы, так как мы используем db.ForeignKey
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    # Note пишется с большой буквы так как мы пишем название класса в db.relationship
+    # db.relationship испольхуется тогда,
+    # когда мы должны настроить множественное отнощение( Один пользователь имеет много постов)
+    notes = db.relationship('Note')
