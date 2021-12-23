a2b08241f8f5b19d2863a071b35429c3dbb61d94
Besenok-13 <artbondar2003@gmail.com>
2021-12-23 15:52:35 +0300

    Final BaseUnderGround

--- website/views.py
+++ website/views.py
@@ -5,9 +5,9 @@ Created on Wed Oct 13 21:16:55 2021
 @author: artbo
 """
 
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, User
 from . import db
 import json
 
@@ -24,7 +24,7 @@ def home():
             new_note=Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash("какой молодчинка, твоя запись была добавлена)")
+            flash("какой молодчинка, твоя запись была добавлена)", category="success")
 
     return render_template("home.html",user=current_user)
 
@@ -38,3 +38,24 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
     return jsonify({})
+"""
+Всё что идёт дальше - бред гения, или просто бред, смотря по работоспособности.
+НАСТОЯТЕЛЬНО НЕ СОВЕТУЮ ЧТО-ЛИБО МЕНЯТЬ, ибо только я знаю, что в каком костыле используется.
+Приятного чтения и поменьше крови из глаз.
+"""
+@views.route("/posts")
+def posts():
+    users = User.query.order_by(User.id).all()
+    return render_template("posts.html",users=users, user=current_user)
+
+
+@views.route("/userpost/<int:noteId>/delete")
+def post_del(noteId):
+    note=Note.query.get(noteId)
+    if note:
+        if note.user_id==current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+        else:
+            flash("Ты чорт, чо делаешь не в своём аккаунте?", category="error")
+    return redirect(url_for("views.home"))
